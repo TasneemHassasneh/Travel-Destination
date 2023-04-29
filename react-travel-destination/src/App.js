86de36ac2937db8e6f8710529d2f29e7388b2718
TasneemHassasneh <tasneemhassasneh890@gmail.com>
2023-04-29 20:41:39 +0300

    building a Travel Destination application where you can see a list of the most visited places around the world.

---
+++ react-travel-destination/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Tours from './components/tours/Tour';
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <Tours />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-travel-destination/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header(){
+    return(
+        <div>
+            <h1>My Travel Destinations</h1>
+        </div>
+    );
+}
+
+export default Header;
