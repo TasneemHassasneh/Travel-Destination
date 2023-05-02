8d4b921adcbca65f9943e3e45512faf588ce59b3
TasneemHassasneh <tasneemhassasneh890@gmail.com>
2023-05-02 21:58:00 +0300

    useState, Routing, useParams

--- react-travel-destination/src/components/footer/Footer.js
+++ react-travel-destination/src/components/footer/Footer.js
@@ -1,12 +1,17 @@
 import React from "react";
+import facebook from './facebook (1).png'
+import twitter from './twitter (1).png'
+import './Footer.css'
 
 function Footer(){
     return(
         <footer>
-            <div>
-                <i class="fi fi-brands-facebook"></i>
-            </div>
             <p>Created by Tasneem Hassasneh</p>
+            <div id="footer-container">
+                <a href=".."><img id='icon' src={facebook} alt='facebook'/></a>
+                <a href=".."><img id='icon' src={twitter} alt='facebook'/></a>
+            </div>
+            
         </footer>
     );
 }
--- react-travel-destination/src/components/tours/tour/Tour.js
+++ react-travel-destination/src/components/tours/tour/Tour.js
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './Tour.css'
 
 function Tour({ tour }) {
-  const [showMore, setShowMore] = useState(false);
-
-  const handleShowMore = () => {
-    setShowMore(!showMore);
-  };
-
-  const description = showMore ? tour.description: `${tour.info.substring(0, 50)}...`;
-
+  
+  
   return (
     <div>
         <div id='container'>
         <h2>{tour.name}</h2>
-      <img src={tour.image} alt={tour.name} />
-      <p>{description}</p>
-      <button onClick={handleShowMore}>
-        {showMore ? 'See more' : 'See less'}
-      </button>
-      <Link to={`/city/${tour.id}`}>View Details</Link>
+      <img id="tour-img" src={tour.image} alt={tour.name} />
+      <a id='details-a' href={`/city/${tour.id}`}>View Details</a>
         </div>
-      
     </div>
   );
 }
